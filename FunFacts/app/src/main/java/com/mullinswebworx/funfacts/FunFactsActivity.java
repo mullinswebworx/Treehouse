5db9c88a6856bfae09168cea0681ec7057c4180f
Jon Mullins <km4gig@gmail.com>
2016-06-12 00:03:00 -0400

    Initial commit

---
+++ FunFacts/app/src/main/java/com/mullinswebworx/funfacts/FunFactsActivity.java
@@ -0,0 +1,40 @@
+package com.mullinswebworx.funfacts;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class FunFactsActivity extends AppCompatActivity {
+    private FactBook mFactBook = new FactBook();
+
+    // declare our view variables.
+    private TextView mFactTextView;
+    private Button mShowFactButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fun_facts);
+
+        // Assign the views from the layout file to the corresponding variables
+        mFactTextView = (TextView) findViewById(R.id.factTextView);
+        mShowFactButton = (Button) findViewById(R.id.showFactButton);
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String fact = mFactBook.getFact();
+
+                // Update the screen with our dynamic fact
+                mFactTextView.setText(fact);
+
+            }
+        };
+        mShowFactButton.setOnClickListener(listener);
+
+    }
+}
