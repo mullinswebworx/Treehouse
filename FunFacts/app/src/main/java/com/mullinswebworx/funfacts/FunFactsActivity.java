8f28df542163c90cbfcaa63ed4d74cef481693f9
Jon Mullins <km4gig@gmail.com>
2016-07-02 01:03:52 -0400

    Finished Build a simple Android app

--- FunFacts/app/src/main/java/com/mullinswebworx/funfacts/FunFactsActivity.java
+++ FunFacts/app/src/main/java/com/mullinswebworx/funfacts/FunFactsActivity.java
@@ -3,14 +3,17 @@ package com.mullinswebworx.funfacts;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Random;
 
 public class FunFactsActivity extends AppCompatActivity {
+    public static final String TAG = FunFactsActivity.class.getSimpleName();
     private FactBook mFactBook = new FactBook();
     private ColorWheel mColorWheel = new ColorWheel();
 
@@ -43,6 +46,9 @@ public class FunFactsActivity extends AppCompatActivity {
             }
         };
         mShowFactButton.setOnClickListener(listener);
+
+        //Toast.makeText(FunFactsActivity.this, "Our activity was created", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "We're logging from the OnCreate() method");
 
     }
 }
