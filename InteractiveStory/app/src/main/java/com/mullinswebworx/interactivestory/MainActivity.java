a5dccb46551c277530ec5eac68f6bff3cb40fd4b
Jon Mullins <km4gig@gmail.com>
2016-07-06 00:12:32 -0400

    finished intents and multiple activities

--- InteractiveStory/app/src/main/java/com/mullinswebworx/interactivestory/MainActivity.java
+++ InteractiveStory/app/src/main/java/com/mullinswebworx/interactivestory/MainActivity.java
@@ -1,5 +1,6 @@
 package com.mullinswebworx.interactivestory;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -24,9 +25,14 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                String name = mNameField.getText().toString();
-                Toast.makeText(MainActivity.this, name, Toast.LENGTH_LONG).show();
+                startStory(name);
             }
         });
+    }
+    private void startStory(String name) {
+        Intent intent = new Intent(this, StoryActivity.class);
+        intent.putExtra(getString(R.string.key_name), name);
+        startActivity(intent);
     }
 }
 
